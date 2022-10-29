d18a46630fbecc0599f1a01f84be9a49fa82449f
João Victo Dantas <67969013+Arsene1999@users.noreply.github.com>
2022-10-29 12:13:14 -0300

    até a aula 183

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -34,3 +34,9 @@ export const IconeLua = (tamanho = 6) => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
     </svg>
 )
+
+export const IconeWarning = (tamanho = 6) => (
+    <svg xmlns="http://www.w3.org/2000/svg" className={`h-${tamanho} w-${tamanho}`} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+    </svg>
+)
--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,18 +1,13 @@
 import Layout from '../components/template/Layout';
-import { AppConsumer } from '../data/context/AppContext';
-import useAppData from '../data/hook/useAppData';
 
 export default function Notificacoes() {
-  
-  const {tema, alternarTema} = useAppData();
   
   return (
     <Layout 
       titulo={'Notificações'} 
       subtitulo={'Aqui você irá gerenciar as suas notificações!'}
     >
-      <h3>{tema}</h3>
-      <button onClick={alternarTema}>Click</button>
+      <h1>Notificações</h1>
     </Layout>
   )
 }
